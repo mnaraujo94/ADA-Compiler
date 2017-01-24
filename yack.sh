4d582d019eaf77e703c206efac23d8295a52d870
mnaraujo94 <mnarau17@g.holycross.edu>
2017-01-23 20:22:03 -0500

    Update and rename yack1.sh to yack.sh

--- yack.sh
+++ yack.sh
@@ -2,7 +2,7 @@
 gcc -o linked_list linked_list.h
 gcc -o symbol_table symbol_table.h
 gcc -o code_generation code_generation.h
-yacc -d -v ada1.y
+yacc -d -v ada.y
 lex ada.l
 /usr/bin/gcc lex.yy.c y.tab.c -o ada -ll -ly
 rm -f lex.yy.c y.tab.c linked_list symbol_table code_generation y.output y.tab.h
